81734f2a8e2a03fdbacb81db5f4f906f428d3705
Megarekrut65 <55782081+Megarekrut65@users.noreply.github.com>
2022-05-14 14:17:07 +0300

    Fixed algorithm
    Now it works for all data length

--- Lab2/encrypt_algorithm/des_cbc/descbc.cpp
+++ Lab2/encrypt_algorithm/des_cbc/descbc.cpp
@@ -1,37 +1,56 @@
-#include <bitset>
 #include "descbc.h"
 
-namespace des{
+namespace des {
+    const uint64_t DESCBC::expander = 0xFFFFFFFFFFFFFFFF;
 
-    DESCBC::DESCBC(const std::vector<uint64_t> &sub_keys) :sub_keys(sub_keys), des{DES(sub_keys)}{}
+    DESCBC::DESCBC(const std::vector<uint64_t> &sub_keys) : sub_keys(sub_keys), des{DES(sub_keys)} {}
 
     void DESCBC::encrypt_input(std::istream &in, std::ostream &out, uint64_t iv) {
-        uint64_t length = in.tellg();
+        std::streamsize length = in.tellg();
         in.seekg(0, std::ios::beg);
         uint64_t buffer = 0;
         uint64_t prev_c = iv;
-        uint64_t current_m;
-        std::cout << length << std::endl;
-        for (uint64_t i = 0; i < length / 8; i++) {
+        std::streamsize size = length % 8 == 0 ? length / 8 - 1 : length / 8;
+        for (std::streamsize i = 0; i < size; i++) {
             in.read((char *) &buffer, 8);
-            current_m = buffer;
-            prev_c = des.encrypt(current_m ^ prev_c);
+            prev_c = des.encrypt(buffer ^ prev_c);
             out.write((char *) &prev_c, 8);
         }
+        std::streamsize part = length % 8;
+        buffer = 0;
+        in.read((char *) &buffer, part != 0 ? part : 8);
+        buffer ^= expander;
+        prev_c = des.encrypt(buffer ^ prev_c);
+        out.write((char *) &prev_c, 8);
     }
+
     void DESCBC::decrypt_input(std::istream &in, std::ostream &out, uint64_t iv) {
-        uint64_t length = in.tellg();
-        std::cout << length << std::endl;
+        std::streamsize length = in.tellg();
         in.seekg(0, std::ios::beg);
         uint64_t buffer = 0;
         uint64_t c_prev = iv;
-        uint64_t p_curr;
-        for (uint64_t i = 0; i < length / 8; i++) {
+        uint64_t res;
+        for (std::streamsize i = 0; i < length / 8 - 1; i++) {
             in.read((char *) &buffer, 8);
-            p_curr = buffer;
-            uint64_t res = des.decrypt(p_curr) ^c_prev;
+            res = des.decrypt(buffer) ^ c_prev;
             out.write((char *) &res, 8);
-            c_prev = p_curr;
+            c_prev = buffer;
         }
+        in.read((char *) &buffer, 8);
+        res = des.decrypt(buffer) ^ c_prev;
+        res ^= expander;
+        out.write((char *) &res, (std::streamsize) get_number_real_byte_size(res));
+    }
+
+    std::size_t DESCBC::get_number_real_byte_size(uint64_t value) {
+        std::stringstream stream;
+        stream << std::bitset<64>(value);
+        std::string str = stream.str();
+        std::size_t index = 0;
+        while (str[index] != '1' && !str.empty()) {
+            str = str.substr(1, str.length());
+        }
+        std::size_t size = str.size();
+        return size % 8 == 0 ? size / 8 : size / 8 + 1;
     }
 }
--- Lab2/encrypt_algorithm/des_cbc/descbc.h
+++ Lab2/encrypt_algorithm/des_cbc/descbc.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "../des/des.h"
 #include <iostream>
+#include <bitset>
 
 namespace des{
     class DESCBC {
+        static const uint64_t expander;
         const std::vector<uint64_t> &sub_keys;
         DES des;
+        std::size_t get_number_real_byte_size(uint64_t value);
     public:
         explicit DESCBC(const std::vector<uint64_t> &sub_keys);
         void encrypt_input(std::istream& in, std::ostream& out, uint64_t iv);
